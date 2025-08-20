3adf6b6fc3a39a0615e598430a0dd2be6071af86
rafarisson <rafarisson@gmail.com>
2025-08-20 16:32:39 -0300

    refactor: deleted old files

--- lib/providers/karaoke_current_singer_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/karaoke_singer.dart';
-
-final karaokeCurrentSinger = Provider<KaraokeSinger>(
-  (ref) => throw UnimplementedError(),
-);
